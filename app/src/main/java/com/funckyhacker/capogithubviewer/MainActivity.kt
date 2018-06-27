4e0115deda57bf6c3122209950b6d035bb923ad0
Capotasto <rocker519804@gmail.com>
2018-06-27 14:42:45 -0700

    init commit

---
+++ app/src/main/java/com/funckyhacker/capogithubviewer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.funckyhacker.capogithubviewer
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
